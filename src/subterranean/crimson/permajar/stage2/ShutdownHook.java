3753cb94463afd46a3ddc75112467752f7fa5461
Tyler Cook <admin@subterranean-security.com>
2015-01-08 02:31:40 -0600

    Updated Copyright

--- src/subterranean/crimson/permajar/stage2/ShutdownHook.java
+++ src/subterranean/crimson/permajar/stage2/ShutdownHook.java
@@ -1,23 +1,23 @@
+/*******************************************************************************
+ *              Crimson Extended Administration Tool (CrimsonXAT)              *
+ *                   Copyright (C) 2015 Subterranean Security                  *
+ *                                                                             *
+ *     This program is free software: you can redistribute it and/or modify    *
+ *     it under the terms of the GNU General Public License as published by    *
+ *      the Free Software Foundation, either version 3 of the License, or      *
+ *                      (at your option) any later version.                    *
+ *                                                                             *
+ *       This program is distributed in the hope that it will be useful,       *
+ *        but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *                 GNU General Public License for more details.                *
+ *                                                                             *
+ *      You should have received a copy of the GNU General Public License      *
+ *      along with this program.  If not, see http://www.gnu.org/licenses      *
+ *******************************************************************************/
 package subterranean.crimson.permajar.stage2;
 
-/*
- * 	Crimson Extended Administration Tool
- *  Copyright (C) 2015 Subterranean Security
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+
 
 import subterranean.crimson.permajar.stage1.network.Communications;
 import subterranean.crimson.permajar.stage2.modules.keylogger.Keylogger;
--- src/subterranean/crimson/server/sdk/API.java
+++ src/subterranean/crimson/server/sdk/API.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ *              Crimson Extended Administration Tool (CrimsonXAT)              *
+ *                   Copyright (C) 2015 Subterranean Security                  *
+ *                                                                             *
+ *     This program is free software: you can redistribute it and/or modify    *
+ *     it under the terms of the GNU General Public License as published by    *
+ *      the Free Software Foundation, either version 3 of the License, or      *
+ *                      (at your option) any later version.                    *
+ *                                                                             *
+ *       This program is distributed in the hope that it will be useful,       *
+ *        but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *                 GNU General Public License for more details.                *
+ *                                                                             *
+ *      You should have received a copy of the GNU General Public License      *
+ *      along with this program.  If not, see http://www.gnu.org/licenses      *
+ *******************************************************************************/
 package subterranean.crimson.server.sdk;
 
 import subterranean.crimson.server.Server;
@@ -9,24 +26,7 @@ import subterranean.crimson.server.sdk.interfaces.GPFrame;
 import subterranean.crimson.server.sdk.interfaces.SPFrame;
 import subterranean.crimson.universal.containers.PluginMessage;
 
-/*
- * 	Crimson Extended Administration Tool
- *  Copyright (C) 2015 Subterranean Security
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- */
+
 
 /**
  * @author globalburning Provides one place for many common tasks plugins need to preform
